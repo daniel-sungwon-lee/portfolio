0b007d65305422e9765c9c06ce5110c0171cf16d
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-19 23:39:00 -0800

    section edits and added home

--- src/home.jsx
+++ src/home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Contact(props) {
+export default function Home(props) {
   return (
     <div></div>
   )
--- src/page.jsx
+++ src/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import Nav from './nav'
+import Nav from './nav';
+import Home from './home';
+import About from './about';
+import Projects from './projects';
+import Skills from './skills';
+import Contact from './contact';
 
 export default class Page extends React.Component {
   constructor(props) {
@@ -13,8 +18,14 @@ export default class Page extends React.Component {
 
   render(){
     return (
-      <Nav />
-
+      <>
+        <Nav />
+        <Home />
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </>
     )
   }
 }
