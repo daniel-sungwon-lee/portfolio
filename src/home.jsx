d1b0bc3b1cc4efaa525d95a880d7dc3b373ac7bf
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-20 15:45:32 -0800

    added layout and polished typewriter

--- src/home.jsx
+++ src/home.jsx
@@ -3,32 +3,44 @@ import TypeWriter from 'typewriter-effect';
 
 export default function Home(props) {
   return (
-    <div className="home" id="home">
+    <div className="container home" id="home">
       <h1 className="typewriter">
         <TypeWriter
           options={
             {
               strings:[
-                `<i style="color: #66d9ef">function</i> <span style="color: #a6e22e">Person</span>(<i style="color: #fd971f">fullName</i>, <i style="color: #fd971f">role</i>) { <br>
-                    this.fullName = fullName; <br>
-                    this.role = role; <br>
-                }; <br>
-                <br>
-                <i style="color: #66d9ef">const</i> daniel = <span style="color: #f92672">new</span> <span style="color: #a6e22e">Person</span>("Daniel Sungwon Lee", "Developer");`,
+                `Hello there!`
               ],
               autoStart:true,
               pauseFor: 3000,
-              deleteSpeed: 33,
+              deleteSpeed: 0,
               loop: true
             }
           }
         />
+        <TypeWriter
+          onInit={
+            typewriter =>{
+              typewriter.typeString(`<i style="color: #66d9ef">function</i> <span style="color: #a6e22e">Person</span>(<i style="color: #fd971f">fullName</i>, <i style="color: #fd971f">role</i>) { <br>`)
+              .pauseFor(300)
+              .typeString(`<span style="padding-left: 2rem">this.fullName = fullName;</span> <br>`)
+              .pauseFor(300)
+              .typeString(`<span style="padding-left: 2rem">this.role = role;</span> <br> };`)
+              .pauseFor(333)
+              .typeString(`<br> <br>`)
+              .typeString(`<i style="color: #66d9ef">const</i> daniel = <span style="color: #f92672">new</span> <span style="color: #a6e22e">Person</span>("Daniel Sungwon Lee", "Developer");`)
+              .pauseFor(5000)
+              .deleteAll(3)
+              .start()
+            }
+          }
+          options={
+            {
+              loop:true
+            }
+          }
+          />
       </h1>
     </div>
   )
 }
-function Person (name, role) {
-  this.name = name;
-  this.role
-}
-new Person("Daniel",)
