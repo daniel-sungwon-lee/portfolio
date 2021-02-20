40f1beab1eacfdd53a2d77aaff346a3e9ff21edf
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-19 22:31:04 -0800

    added sections

--- src/nav.jsx
+++ src/nav.jsx
@@ -5,26 +5,40 @@ const black = {
   background: "black"
 }
 
-export default function Nav(props){
-  return (
-    <div className="fixed-top">
-      <nav className="navbar border-0" style={black}>
-        <div data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <Hamburger rounded direction="left" color="rgba(255,255,255,.5)" />
-        </div>
-      </nav>
-      <div className="collapse position-relative border-0" id="navbarToggleExternalContent">
-        <div className="p-4 pb-5 border-0" style={black}>
-          <a href="#home" className="text-decoration-none"><h5 className="text-white h4 nav-links" data-toggle="collapse" data-target="#navbarToggleExternalContent">Home</h5></a>
-          <a href="#about" className="text-decoration-none"><h5 className="text-white h4 nav-links" data-toggle="collapse" data-target="#navbarToggleExternalContent">About</h5></a>
-          <a href="#projects" className="text-decoration-none"><h5 className="text-white h4 nav-links" data-toggle="collapse" data-target="#navbarToggleExternalContent">Projects</h5></a>
-          <a href="#skills" className="text-decoration-none"><h5 className="text-white h4 nav-links" data-toggle="collapse" data-target="#navbarToggleExternalContent">Skills</h5></a>
-          <a href="#contact" className="text-decoration-none"><h5 className="text-white h4 nav-links" data-toggle="collapse" data-target="#navbarToggleExternalContent">Contact</h5></a>
-        </div>
-        <div className="username-greeting">
-          <h4>{`Hello there!`}</h4>
+export default class Nav extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      toggled: false
+    }
+    this.handleHamburger=this.handleHamburger.bind(this)
+  }
+
+  handleHamburger() {
+    this.setState({toggled: false})
+  }
+
+  render(){
+    return (
+      <>
+      <div className="collapse overlay" onClick={this.handleHamburger} id="navbarToggleExternalContent" data-toggle="collapse" data-target="#navbarToggleExternalContent"></div>
+      <div className="fixed-top">
+        <nav className="navbar border-0" style={black}>
+          <div data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <Hamburger toggled={this.state.toggled} toggle={this.state.toggled} rounded direction="right" color="white" />
+          </div>
+        </nav>
+        <div className="collapse position-relative border-0" id="navbarToggleExternalContent">
+          <div className="p-4 pb-5 border-0" style={black}>
+            <a href="#" className="text-decoration-none"><h5 className="h4 nav-links" onClick={this.handleHamburger} data-toggle="collapse" data-target="#navbarToggleExternalContent">Home</h5></a>
+            <a href="#about" className="text-decoration-none"><h5 className="h4 nav-links" onClick={this.handleHamburger} data-toggle="collapse" data-target="#navbarToggleExternalContent">About</h5></a>
+            <a href="#projects" className="text-decoration-none"><h5 className="h4 nav-links" onClick={this.handleHamburger} data-toggle="collapse" data-target="#navbarToggleExternalContent">Projects</h5></a>
+            <a href="#skills" className="text-decoration-none"><h5 className="h4 nav-links" onClick={this.handleHamburger} data-toggle="collapse" data-target="#navbarToggleExternalContent">Skills</h5></a>
+            <a href="#contact" className="text-decoration-none"><h5 className="h4 nav-links" onClick={this.handleHamburger} data-toggle="collapse" data-target="#navbarToggleExternalContent">Contact</h5></a>
+          </div>
         </div>
       </div>
-    </div>
-  )
+      </>
+    )
+  }
 }
