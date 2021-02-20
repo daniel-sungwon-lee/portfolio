db9745f2b1dba32584be797f5c20513ce8466ccd
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-19 23:23:02 -0800

    edits made

--- src/nav.jsx
+++ src/nav.jsx
@@ -14,16 +14,16 @@ export default function Nav(props) {
     <div className="fixed-top">
       <nav className="navbar border-0" style={black}>
         <div data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-          <Hamburger toggled={isOpen} toggle={setOpen} rounded duration={0.5} direction="right" color="white" />
+          <Hamburger toggled={isOpen} toggle={setOpen} rounded easing="ease-out" duration={0.4} direction="right" color="white" />
         </div>
       </nav>
       <div className="collapse position-relative border-0" id="navbarToggleExternalContent">
         <div className="p-4 pb-5 border-0" style={black}>
-            <a href="#" className="text-decoration-none"><h5 className="h4 nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Home</h5></a>
-            <a href="#about" className="text-decoration-none"><h5 className="h4 nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">About</h5></a>
-            <a href="#projects" className="text-decoration-none"><h5 className="h4 nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Projects</h5></a>
-            <a href="#skills" className="text-decoration-none"><h5 className="h4 nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Skills</h5></a>
-            <a href="#contact" className="text-decoration-none"><h5 className="h4 nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Contact</h5></a>
+            <a href="#" className="text-decoration-none"><h3 className="nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Home</h3></a>
+            <a href="#about" className="text-decoration-none"><h3 className="nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">About</h3></a>
+            <a href="#projects" className="text-decoration-none"><h3 className="nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Projects</h3></a>
+            <a href="#skills" className="text-decoration-none"><h3 className="nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Skills</h3></a>
+            <a href="#contact" className="text-decoration-none"><h3 className="nav-links" onClick={() => setOpen(false)} data-toggle="collapse" data-target="#navbarToggleExternalContent">Contact</h3></a>
         </div>
       </div>
     </div>
