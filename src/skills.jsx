205e51e5fe82e3ad22c4408c610fd1a3bc2f055e
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-21 22:58:05 -0800

    modified files

--- src/skills.jsx
+++ src/skills.jsx
@@ -2,19 +2,68 @@ import React from 'react';
 
 export default function Skills(props) {
   return (
-    <div className="container" id="skills">
-      <div className="d-flex justify-content-around align-items-center">
+    <div className="container skills" id="skills">
+      <h1 className="text-center">Skills</h1>
+      <div className="mt-5 d-flex flex-wrap justify-content-around align-items-center">
         <div className="skills-icon">
-          <img width="100" src="icons-colored/javascript.svg"></i>
+          <img width="100" src="icons-colored/javascript.svg"/>
         </div>
         <div className="skills-icon">
-          <i class="fab fa-react"></i>
+          <img width="100" src="icons-colored/react.svg"/>
         </div>
         <div className="skills-icon">
-          <i class="fab fa-css3-alt"></i>
+          <img width="100" src="icons-colored/css.svg"/>
         </div>
         <div className="skills-icon">
-          <i class="fab fa-js-square"></i>
+          <img width="100" src="icons-colored/html.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/bootstrap.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="163.3" src="icons-colored/node.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="180" src="icons-colored/express.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="97.03" src="icons-colored/postgresql.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="152" src="icons-colored/npm.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/webpack.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="220.2" src="icons-colored/babel.svg"/>
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/git.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="66.7" src="icons-colored/figma.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/vs-code.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/bash.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/json.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="90" src="icons-colored/heroku.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="102.58" src="icons-colored/photoshop.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="100" src="icons-colored/netlify.svg" />
+        </div>
+        <div className="skills-icon">
+          <img width="99.82" src="icons-colored/slack.svg" />
         </div>
       </div>
     </div>
