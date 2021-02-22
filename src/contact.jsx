0aceca9b518ca96a8a10532efd3b05a2f94bfcb9
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-22 00:14:10 -0800

    added contact section

--- src/contact.jsx
+++ src/contact.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 
 export default function Contact(props) {
   return (
-    <div></div>
+    <div className="container contact" id="contact">
+      <h1 className="text-center">Contact</h1>
+      <div className="mt-5 d-flex justify-content-around align-items-center">
+        <div className="contact-logo">
+          <a href="https://www.linkedin.com/in/daniel-sungwon-lee/" target="_blank" className="text-decoration-none" style={{color: "#0077B5"}}>
+            <i className="fab fa-linkedin"></i>
+          </a>
+        </div>
+        <div className="contact-logo">
+          <a href="https://github.com/daniel-sungwon-lee/" target="_blank" className="text-decoration-none" style={{color: "black"}}>
+            <i className="fab fa-github"></i>
+          </a>
+        </div>
+        <div className="contact-logo gmail">
+          <a href="mailto:danlee333@gmail.com">
+            <img src="icons-colored/gmail.svg" width="100"/>
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
