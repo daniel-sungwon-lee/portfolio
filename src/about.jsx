83522bb78e3e2c514558c72ba8cd5cad65a488a8
Daniel Sungwon Lee <danlee333@gmail.com>
2021-02-23 23:50:14 -0800

    edited about me

--- src/about.jsx
+++ src/about.jsx
@@ -20,12 +20,11 @@ export default function About(props){
               <p>
               After high school, I decided to enter a
               7-year pharmacy program to become a pharmacist.
-              But after my bachelor's, I decided that I didn’t want to pursue
-              this path in my life and decided to take an audacious leap
-              of faith into the tech industry; I decided to follow
-              my heart and that part of me that always wanted to be more
-              creative. Plus, it also saved me from accumulating
-              six figure student loans.
+              But after my bachelor's, I realized that pharmacy school was getting
+              too expensive and I didn't have enough passion for it; it only took me 4 years to
+              realize this. And so, I took a leap of faith and decided to follow my
+              heart and that part of me that always wanted to be more creative.
+              Plus, it also saved me from accumulating six figure student loans.
               </p>
               <p>
               Through LearningFuze's full immersion coding bootcamp, I started
@@ -44,7 +43,6 @@ export default function About(props){
             <h3 className="font-weight-bold mt-5">Some of my interests (other than coding)...</h3>
             <h4 className="text">
               <ul>
-                <li>God; the one and only</li>
                 <li>Nature, especially the mountains; a nice break from the world</li>
                 <li>Cooking from scratch; going to the grocery store and bringing items together to put in my mouth</li>
                 <li>Reading the Nutrition Facts label; the more you know, the more you don't want put that stuff in your body</li>
@@ -52,9 +50,9 @@ export default function About(props){
                 <li>Investing; making money is one thing, but knowing how to use it is another</li>
                 <li>Good design; whether that be websites, apps, logos, icons, cars, credit cards, debit cards, etc. (I like collecting credit/debit cards)</li>
                 <li>Minimalism; you really don't need that much in life</li>
-                <li>Films; LOTR, Star Wars, Breaking Bad, The Dark Knight, Catch Me If You Can, Pulp Fiction, Forrest Gump, The Pursuit of Happyness, to name a few...</li>
+                <li>Films/music; I enjoy and appreciate fine arts</li>
                 <li>Intellectual, inspirational, motivational, and thought provoking quotes; also made an app that generates random quotes and other stuff, check it out <a href="https://daniel-sungwon-lee.github.io/ajax-project/" target="_blank" className="text-decoration-none">here!</a> (sorry I'll stop)</li>
-                <li>Memes; from <a href="https://www.reddit.com/" target="_blank" className="fab fa-reddit text-decoration-none" style={{color: "#FF4301"}}></a></li>
+                <li>Memes; mainly from <a href="https://www.reddit.com/" target="_blank" className="fab fa-reddit text-decoration-none" style={{color: "#FF4301"}}></a></li>
                 <li>Semicolons; in English, not so much in JavaScript</li>
               </ul>
             </h4>
