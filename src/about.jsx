25ab8af808158c3febc69e3d749202aacdb4258e
Daniel Sungwon Lee <danlee333@gmail.com>
2022-11-07 10:42:46 -0800

    about me edit

--- src/about.jsx
+++ src/about.jsx
@@ -11,45 +11,36 @@ export default function About(props){
             <h1 className="text-center">
               About Me
             </h1>
-            <h3 className="font-weight-bold mt-5">My story...</h3>
+            <h3 className="font-weight-bold mt-5 mb-2">My story...</h3>
             <h4 className="text">
-              <Skeleton>
                 <p>
                 Life is full of surprises; no wonder it is like a box
-                of chocolates. My path and destination to becoming
-                a web developer was definitely an unexpected journey.
+                of chocolates. My path to where I am is definitely an
+                unexpected journey.
                 </p>
-              </Skeleton>
-              <Skeleton>
                 <p>
-                After high school, I decided to enter a
-                7-year pharmacy program to become a pharmacist.
-                But after my bachelor's, I realized that pharmacy school was getting
-                too expensive and I didn't have enough passion for it; yes, it only took me
-                4 years to realize this. And so, I took a leap of faith and decided
-                to follow my heart and that part of me that always wanted to be more
-                creative. Plus, it also saved me from accumulating six figure student loans.
+                After deciding to enter a pharmacy program to become a pharmacist,
+                I realized that pharmacy school was getting too expensive and I didn't have
+                enough passion for it; yes, it only took me 4 years and a bachelor's to realize this.
+                And so, I took a leap of faith and decided to switch things up a bit and try out
+                software engineering, studying and learning how computers/software work instead of how
+                humans work for a living. Plus, it also saved me from accumulating six figure student loans.
                 </p>
-              </Skeleton>
-              <Skeleton>
                 <p>
-                Through a full immersion coding bootcamp, I started
-                my path into the software industry where problem solving, creativity,
-                innovation, development, improvement, and the desire to
-                learn new things are some of the key attributes of
-                becoming a great engineer. Since I love to figure things
-                out and make/build things through logic, making this change felt right.
-                Also, software is all around us and makes life much more convenient.
+                After an arduous period of learning and applying, I started
+                my path in software development in the advertising industry, working and helping out
+                on the tech side of things since most ads and marketing nowadays are done digitally.
+                It was a great period of applying my knowledge and learning new technologies, figuring things out,
+                and developing/contributing to a myriad of fun projects from different clients; overall, it was a
+                great period of real world experience.
                 </p>
-              </Skeleton>
-              <Skeleton>
                 <p>
-                My tech stack consists of JavaScript, React, HTML, CSS,
-                Node, etc. and I am open to any opportunities, eager to
-                apply my skills and learn new things; to go from idea to software
-                through code.
+                I am a fast learner, adaptable, analytical, detail oriented, reliable, and a natural
+                problem solver. Most importantly, I can just sit down for a long period of time and
+                get things done in a timely manner. I am open for growth and ready to try out other
+                industries, learn new technologies, and be a part of any team to help make life more convenient
+                through software.
                 </p>
-              </Skeleton>
             </h4>
             <h3 className="font-weight-bold mt-5">Some of my interests (other than coding)...</h3>
             <h4 className="text">
@@ -65,6 +56,7 @@ export default function About(props){
                 <li>Photography; it captures a small, tangible moment and memory in life</li>
                 <li>Intellectual, inspirational, motivational, and thought provoking quotes; I also made an app that generates random quotes and other stuff, check it out <a href="https://daniel-sungwon-lee.github.io/ajax-project/" target="_blank" className="text-decoration-none">here!</a> (sorry I'll stop)</li>
                 <li>Semicolons (if you haven't realized by now); I like them in English, not so much in JavaScript (as you can tell by looking at some of my <a href="https://github.com/daniel-sungwon-lee?tab=repositories" target="blank" className="text-decoration-none">code</a>)</li>
+                <li>Thrifting; you never know what gems you might find at a fraction of the cost, since clothes can get pretty expensive.</li>
               </ul>
             </h4>
           </div>
